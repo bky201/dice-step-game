955d2e184836d09bb628be66555c2314dcc98c76
bky <bekrye@gmail.com>
2023-05-24 22:24:41 +0200

    Add home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// Wait for page to fiish loading generate a random nnumber on each box
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    // let hexagons = document.getElementsByClassName("hexagon");
+
+    // for (let hexagon of hexagons) {
+    //     hexagon.textContent = Math.floor(Math.random()*6)+1;
+    // }
+})
 
 function displayBoard() {
 
