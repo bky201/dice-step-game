fa4a38fea9f9c8f485d90e22d62691492932fd90
bky <bekrye@gmail.com>
2023-05-25 18:48:29 +0200

    Add event listener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 
-// Wait for page to fiish loading generate a random nnumber on each box
+// Loading the page and add event listner to the buttons page
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.getElementsByTagName("button");
 
+    for (const button of buttons) {
 
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "new-game"){
+                alert("You clicked new-game");
+            } else {
+                let buttonType = this.getAttribute("data-type");
+                alert(`You clicked ${buttonType}`);
+            }
+        })
+    }
+})
 
 
 function displayBoard() {
