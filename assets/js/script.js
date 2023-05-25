bc180979ea97d76e2ec894754c8f2be8a21b933a
bky <bekrye@gmail.com>
2023-05-25 20:16:01 +0200

    Add code to display numbers to hexagon board

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "new-game"){
-                alert("You clicked new-game");
+                // alert("You clicked new-game");
             } else {
                 let buttonType = this.getAttribute("data-type");
-                alert(`You clicked ${buttonType}`);
+                // alert(`You clicked ${buttonType}`);
             }
         })
     }
 })
 
-
+/**
+ * displayBoard is called when the user starts
+ * new game or resets a game
+ */
 function displayBoard() {
-
+    const hexagons = document.getElementsByClassName("hexagon");
+    for (const hexagon of hexagons) {
+        hexagon = Math.floor(Math.random()*6)+1;
+    }
 }
 
 function playGame() {
