7ec7727edcdee8b0e18fb3525beb203a5bda0da7
bky <bekrye@gmail.com>
2023-05-25 23:37:06 +0200

    Add code to dispalyboard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.getElementsByTagName("button");
 
-    for (const button of buttons) {
+    for (let button of buttons) {
 
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "new-game"){
-                // alert("You clicked new-game");
+               displayBoard("hexagon"); 
             } else {
                 let buttonType = this.getAttribute("data-type");
                 // alert(`You clicked ${buttonType}`);
             }
         })
     }
+
+    displayBoard("hexagon")
+    
 })
 
 /**
  * displayBoard is called when the user starts
  * new game or resets a game
  */
-function displayBoard() {
-    const hexagons = document.getElementsByClassName("hexagon");
-    for (const hexagon of hexagons) {
-        hexagon = Math.floor(Math.random()*6)+1;
+function displayBoard(className) {
+    let hexagons = document.getElementsByClassName("hexagon");
+    for (let i = 0; i < hexagons.length; i++) {
+        const number = hexagons[i].querySelector('p');
+        number.innerHTML = Math.floor(Math.random()*6)+1;
     }
 }
 
